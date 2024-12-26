efc3af560011556c1459c41c9a2552f0856979be
marinalaym <mlaforgithub@gmail.com>
2024-12-26 17:45:32 +0300

    fix

--- vetclinic/src/main/java/ru/marinalyamina/vetclinic/apicontrollers/AppointmentApiController.java
+++ vetclinic/src/main/java/ru/marinalyamina/vetclinic/apicontrollers/AppointmentApiController.java
@@ -1,14 +1,15 @@
 package ru.marinalyamina.vetclinic.apicontrollers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.marinalyamina.vetclinic.models.entities.AnimalType;
 import ru.marinalyamina.vetclinic.models.entities.Appointment;
-import ru.marinalyamina.vetclinic.services.AnimalTypeService;
 import ru.marinalyamina.vetclinic.services.AppointmentService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/appointments")
@@ -24,4 +25,22 @@ public class AppointmentApiController {
     public List<Appointment> getAll() {
         return appointmentService.getAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Appointment> getAppointmentById(@PathVariable Long id) {
+        Optional<Appointment> appointmentOptional = appointmentService.getById(id);
+
+        if (appointmentOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        try{
+            appointmentOptional.get().initFiles();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        return ResponseEntity.ok(appointmentOptional.get());
+    }
 }
+
